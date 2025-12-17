40ccd483e71b4b5d70a2678364a8a5e32bcd351b
arcctg <91179695+arcctg@users.noreply.github.com>
2025-12-17 07:14:54 +0200

    refactor: update DirtiesContext configuration in SubscriptionExpirationSchedulerTest
    Modified the DirtiesContext annotation in SubscriptionExpirationSchedulerTest to use ClassMode.BEFORE_CLASS, improving test performance and isolation by reducing the overhead of resetting the context before each test method.

--- streaming-service/src/test/java/dev/studentpp1/streamingservice/subscription/service/SubscriptionExpirationSchedulerTest.java
+++ streaming-service/src/test/java/dev/studentpp1/streamingservice/subscription/service/SubscriptionExpirationSchedulerTest.java
@@ -24,12 +24,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
 class SubscriptionExpirationSchedulerTest extends AbstractPostgresContainerTest {
 
     @Autowired
